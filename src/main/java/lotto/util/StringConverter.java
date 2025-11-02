b9b607891841cc9539939141dea7509c8d61dc28
kimdanha <lily034686@gmail.com>
2025-11-02 23:05:10 +0900

    feat: StringConverter 클래스 추가

---
+++ src/main/java/lotto/util/StringConverter.java
@@ -0,0 +1,31 @@
+package lotto.util;
+
+import lotto.config.ErrorMessage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StringConverter {
+
+    public static List<Integer> toIntList(String input) {
+        validateDelimiter(input);
+
+        return Arrays.stream(input.split(","))
+                .map(String::trim)
+                .peek(StringConverter::validateNumber)
+                .map(Integer::parseInt)
+                .toList();
+    }
+
+    private static void validateDelimiter(String input) {
+        if (!input.contains(",")) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_DELIMITER.getMessage());
+        }
+    }
+
+    private static void validateNumber(String token) {
+        if (!token.matches("\\d+")) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER_INPUT.getMessage());
+        }
+    }
+}
