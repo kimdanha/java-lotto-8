2e3ba122132877719c2773ff6c9eabea99447927
kimdanha <lily034686@gmail.com>
2025-11-02 23:27:55 +0900

    feat: Application 작성

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,13 @@
 package lotto;
 
+import lotto.controller.LottoGameController;
+import lotto.service.LottoService;
+import lotto.domain.LottoGenerator;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        LottoService lottoService = new LottoService(new LottoGenerator());
+        LottoGameController controller = new LottoGameController(lottoService);
+        controller.run();
     }
 }
