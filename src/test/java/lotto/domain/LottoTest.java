b378c4046fe4978bdf1fd3c0f3557a800f85fe6f
kimdanha <lily034686@gmail.com>
2025-11-02 21:23:24 +0900

    test: Lotto 일치하는 숫자 개수, 특정 번호 포함되어있는지 확인하는 테스트 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class LottoTest {
 
@@ -31,5 +31,25 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+    }
+
+    @DisplayName("다른 로또와 비교했을 때 일치하는 숫자의 개수를 반환한다.")
+    @Test
+    void countMatchingNumbers_일치숫자반환() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        Lotto other = new Lotto(List.of(3, 4, 5, 7, 8, 9));
+
+        int matchCount = lotto.countMatchingNumbers(other);
+
+        assertThat(matchCount).isEqualTo(3);
+    }
+
+    @DisplayName("특정 숫자가 로또 번호에 포함되어 있는지 확인한다.")
+    @Test
+    void contains_포함여부확인() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        assertThat(lotto.contains(3)).isTrue();
+        assertThat(lotto.contains(10)).isFalse();
     }
 }
